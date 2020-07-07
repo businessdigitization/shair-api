33bb9cf07a9b73c52ca66cb8b3411f24675659c0
farukin <fah@inscale.net>
2020-07-08 00:34:37 +0800

    Trying configuring rubocop 9

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'validations' do
+describe 'validations' do
     let(:user) { FactoryBot.build(:user, name: 'Faruk', email: 'faruk@gmail.com') }
 
     it 'is not valid without name' do
@@ -9,7 +9,7 @@ RSpec.describe User do
       expect(user.valid?).to eq(false )
     end
 
-     it "is not valid without email" do 
+     it "is not valid without email" do
       user.email = nil
       expect(user.valid?).to eq(false )
     end
