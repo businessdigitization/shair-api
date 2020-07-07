94146ab92406f39acccf52f30102239e306b4260
farukin <fah@inscale.net>
2020-07-07 23:14:34 +0800

    Configure neccessary gems and rspec configuration with some spec

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module ShairApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+
+    end
+
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { "Faruk Hossain" }
+    email { "faruk@mail.com" }
+  end
+end
