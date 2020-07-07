047adabc7ba5e92f6e19a3d5499ccf3f31fb6ed3
farukin <fah@inscale.net>
2020-07-08 00:59:46 +0800

    Delete greetings workflow

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User do
 describe 'validations' do
-    let(:user) { FactoryBot.build(:user,name: 'Faruk', email: 'faruk@gmail.com') }
+    let(:user) {FactoryBot.build(:user,name: 'Faruk',email: 'faruk@gmail.com') }
 
     it 'is not valid without name' do
       user.name =nil
       expect(user.valid?).to eq(false )
     end
 
-     it "is not valid without email" do
+        it "is not valid without email" do
       user.email =nil
       expect(user.valid?).to eq(false )
     end
