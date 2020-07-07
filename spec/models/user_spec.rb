0ad8560a4a7b221e5cdd2014571a99d3040f087a
farukin <fah@inscale.net>
2020-07-08 01:39:52 +0800

    Configure action Y

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require   'rails_helper'
 
         RSpec.describe User do
 describe 'validations' do
-    let(:user) {FactoryBot.build(:user,name: 'Faruk',email: 'faruk@gmail.com') }
+    let(:user) {FactoryBot.build(:user,name: 'Faruk',email: 'faruk@gmail.com')}
 
     it 'is not valid without name' do
     user.name=nil
