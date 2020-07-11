1b82e0f0c2c767036e02c33226313b46b64ef850
farukin <fah@inscale.net>
2020-07-11 21:56:09 +0800

    Fix rubocop comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,26 @@
 require   'rails_helper'
 
-        RSpec.describe User do
-describe 'validations' do
-    let(:user) {FactoryBot.build(:user,name: 'Faruk',email: 'faruk@gmail.com')}
+RSpec.describe User do
+  describe 'validations' do
+    let(:user) { FactoryBot.build(:user, name: 'Faruk', email: 'faruk@gmail.com') }
 
     it 'is not valid without name' do
-    user.name=nil
-      expect(user.valid?).to eq(false )
+      user.name = nil
+      expect(user.valid?).to eq(false)
     end
 
-        it "is not valid without email" do
-      user.email= nil
-      expect(user.valid?).to eq(false )
+    it 'is not valid without email' do
+      user.email = nil
+      expect(user.valid?).to eq(false)
+    end
+
+    describe 'email validation' do
+      it { is_expected.to allow_value('test@test.com').for(:email).on(:create) }
+      it { is_expected.not_to allow_value('invalidemailcom').for(:email).on(:create) }
+      it { is_expected.not_to allow_value('test @test.com').for(:email).on(:create) }
+      it { is_expected.not_to allow_value('tést@test.com').for(:email).on(:create) }
+      it { is_expected.not_to allow_value('däni@test.com').for(:email).on(:create) }
+      it { is_expected.not_to allow_value('test@test').for(:email).on(:create) }
     end
   end
 end
