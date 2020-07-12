8166ed490ba8cbaa15066813cc97ded868dbc9a3
farukin <fah@inscale.net>
2020-07-13 00:13:36 +0800

    Prepare app

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, :email, presence: true
+
+  # Original REGEX is this https://github.com/ruby/ruby/blob/ruby_2_5/lib/uri/mailto.rb#L56
+  # We have modified it to not allow email with format like "something@somewhere"
+  EMAIL_REGEXP = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-z]{2,}\z/
+
+  validates :email, format: { with: EMAIL_REGEXP }
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'validations' do
+    let(:user) { FactoryBot.build(:user, name: 'Faruk', email: 'faruk@gmail.com') }
+
+    it 'is not valid without name' do
+      user.name = nil
+      expect(user.valid?).to eq(false)
+    end
+
+    it 'is not valid without email' do
+      user.email = nil
+      expect(user.valid?).to eq(false)
+    end
+
+    describe 'email validation' do
+      it { is_expected.to allow_value('test@test.com').for(:email) }
+      it { is_expected.not_to allow_value('invalidemailcom').for(:email) }
+      it { is_expected.not_to allow_value('test @test.com').for(:email) }
+      it { is_expected.not_to allow_value('tést@test.com').for(:email) }
+      it { is_expected.not_to allow_value('däni@test.com').for(:email) }
+      it { is_expected.not_to allow_value('test@test').for(:email) }
+    end
+  end
+end
